fa59dd387fcd9479dca556eb4b3226775b0d841f
Ashwin Shanker <ashanker@berkeley.edu>
2021-05-04 17:29:05 -0700

    some shit

--- simulation/js/main.js
+++ simulation/js/main.js
@@ -1,82 +1,80 @@
-// // ////@ts-check
-var scene;
-var camera;
-var renderer;
+//@author Omar Shehata. 2015.
+		//We are loading the Three.js library from the cdn here: https://cdnjs.com/libraries/three.js/
+		var scene;
+		var camera;
+		var renderer;
 
-function setup() {
-    scene = new THREE.Scene();
-    camera = new THREE.OrthographicCamera( - window.innerWidth / 2, window.innerWidth / 2, window.innerHeight / 2, - window.innerHeight / 2, 1, 1000 );
-    
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.setPixelRatio(window.devicePixelRatio);
+		function scene_setup(){
+			//This is the basic scene setup
+			scene = new THREE.Scene();
+			var width = window.innerWidth;
+			var height = window.innerHeight;
+			//Note that we're using an orthographic camera here rather than a prespective
+			camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+			camera.position.z = 2;
 
-    document.body.appendChild(renderer.domElement);
-    
-    // const geometry = new THREE.BoxGeometry();
-    // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 });
-    // const cube = new THREE.Mesh(geometry, material);
-    // cube.position.y = 3;
-    
-    
-    // cube.width = 1;
-    
-    // scene.add(cube);
-    
-    camera.position.z = 2;
+			renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+		}
 
-}
+		
+		//Initialize the Threejs scene
+		scene_setup();
+		
+		
+		var bufferScene;
+		var textureA;
+		var textureB;
+		var bufferMaterial;
+		var plane;
+		var bufferObject;
+		var finalMaterial;
+		var quad;
 
-setup();
+		function buffer_texture_setup(){
+			//Create buffer scene
+			bufferScene = new THREE.Scene();
+			//Create 2 buffer textures
+			textureA = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+			textureB = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter} );
+			//Pass textureA to shader
+			bufferMaterial = new THREE.ShaderMaterial( {
+				uniforms: {
+				 bufferTexture: { type: "t", value: textureA },
+				 res : {type: 'v2',value:new THREE.Vector2(window.innerWidth,window.innerHeight)}//Keeps the resolution
+				},
+				fragmentShader: document.getElementById( 'fragShader' ).innerHTML
+			} );
+			plane = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight );
+			bufferObject = new THREE.Mesh( plane, bufferMaterial );
+			bufferScene.add(bufferObject);
 
-// one main texture for displaying on screen and another helper texture for the shader
-var mainTex;
-var helperTex;
+			//Draw textureB to screen 
+			finalMaterial =  new THREE.MeshBasicMaterial({map: textureB});
+			quad = new THREE.Mesh( plane, finalMaterial );
+			scene.add(quad);
+		}
+		buffer_texture_setup();
 
-var customMaterial;
-var plane;
-var mesh;
 
-function createMaterials(){
-    buffer = new THREE.Scene();
+		//Render everything!
+		function render() {
 
-    // main and temp textures for reading/writing state updates
-    mainTex = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight);
-    helperTex = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight);
+		  requestAnimationFrame( render );
+		  
+		   //Draw to textureB
+		  renderer.render(bufferScene,camera,textureB,true);
+			
+		  //Swap textureA and B
+		  var t = textureA;
+		  textureA = textureB;
+		  textureB = t;
+		  quad.material.map = textureB;
+		  bufferMaterial.uniforms.bufferTexture.value = textureA;
 
-    var uniforms = {
-        dimensions : {type: 'v2', value: new THREE.Vector2(window.innerWidth,window.innerHeight)},
-        smokeTexture: {type: 't', value: helperTex},
-        mousePos : {type: 'v2', value: new THREE.Vector2(0,0)}
-    }
+		  //Finally, draw to the screen
+		  renderer.render( scene, camera );
 
-    customMaterial = new THREE.ShaderMaterial( {
-      uniforms: uniforms,
-      fragmentShader: document.getElementById( 'basicShader' ).innerHTML
-    });
-
-    plane = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight );
-
-    mesh = new THREE.Mesh( plane, customMaterial );
-    scene.add(mesh);
-  }
-
-  createMaterials();
-
-const animate = function() {
-    requestAnimationFrame(animate);
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    // renderer.render(buffer,camera, mainTex, true);
-
-    var temp = mainTex;
-    mainTex = helperTex;
-    helperTex = temp;
-
-    mesh.material.map = mainTex;
-    customMaterial.uniforms.smokeTexture.value = helperTex;
-
-    renderer.render(scene, camera)
-};
-
-animate();
+		}
+		render();
